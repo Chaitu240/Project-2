76837f512bde093a69311b5e53de85b5c614245b
EC2 Default User <ec2-user@ip-172-31-2-158.us-west-1.compute.internal>
2023-12-13 06:39:35 +0000

    Project-2

---
+++ ec2.tf
@@ -0,0 +1,61 @@
+provider "aws" {
+  access_key = "AKIAXTZJCJCAMH466YT2"
+  secret_key = "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"
+  region     = "us-west-1"
+}
+
+resource "aws_instance" "Project-2" {
+  ami           = "ami-0a946c0cc1e558346"
+  instance_type = "t2.micro"
+  
+}
+
+# Create a VPC
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "MyVPC"
+  }
+}
+
+# Create a subnet
+resource "aws_subnet" "my_subnet" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-west-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "MySubnet"
+  }
+}
+
+# Create an Internet Gateway
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "MyIGW"
+  }
+}
+
+# Create a route table
+resource "aws_route_table" "my_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+
+  tags = {
+    Name = "MyRouteTable"
+  }
+}
+
+# Associate the route table with the subnet
+resource "aws_route_table_association" "my_route_table_association" {
+  subnet_id      = aws_subnet.my_subnet.id
+  route_table_id = aws_route_table.my_route_table.id
+}
